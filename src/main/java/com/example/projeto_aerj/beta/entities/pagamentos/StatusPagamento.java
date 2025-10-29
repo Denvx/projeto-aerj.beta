187050d0ef4c9f5d17e9ece9d7c3b570f2558b93
Denvx <denversonalii@gmail.com>
2025-10-29 06:39:59 -0300

    entitie statuspagamento

---
+++ src/main/java/com/example/projeto_aerj/beta/entities/pagamentos/StatusPagamento.java
@@ -0,0 +1,28 @@
+package com.example.projeto_aerj.beta.entities.pagamentos;
+
+public class StatusPagamento {
+
+    private long id;
+    private String nome;
+
+    public StatusPagamento(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
