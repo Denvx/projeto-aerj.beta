71c7eb7c67d620be406bf73b386cfc528f0f3225
Denvx <denversonalii@gmail.com>
2025-10-29 06:26:32 -0300

    ciação das entities Logs e Avisos

---
+++ src/main/java/com/example/projeto_aerj/beta/entities/auditoria/Log.java
@@ -0,0 +1,40 @@
+package com.example.projeto_aerj.beta.entities.auditoria;
+
+import java.time.LocalDateTime;
+
+public class Log {
+
+    private long id;
+    private String acao;
+    private LocalDateTime dataRegistrada;
+
+    public Log(long id, String acao, LocalDateTime dataRegistrada) {
+        this.id = id;
+        this.acao = acao;
+        this.dataRegistrada = dataRegistrada;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAcao() {
+        return acao;
+    }
+
+    public void setAcao(String acao) {
+        this.acao = acao;
+    }
+
+    public LocalDateTime getDataRegistrada() {
+        return dataRegistrada;
+    }
+
+    public void setDataRegistrada(LocalDateTime dataRegistrada) {
+        this.dataRegistrada = dataRegistrada;
+    }
+}
--- src/main/java/com/example/projeto_aerj/beta/entities/notificacoes/Avisos.java
+++ src/main/java/com/example/projeto_aerj/beta/entities/notificacoes/Avisos.java
@@ -1,6 +1,41 @@
 package com.example.projeto_aerj.beta.entities.notificacoes;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Avisos {
 
+    private long id;
+    private String mensagem;
+    private LocalDateTime dataEnvio;
 
+    public Avisos(long id, String mensagem, LocalDateTime dataEnvio) {
+        this.id = id;
+        this.mensagem = mensagem;
+        this.dataEnvio = dataEnvio;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataEnvio() {
+        return dataEnvio;
+    }
+
+    public void setDataEnvio(LocalDateTime dataEnvio) {
+        this.dataEnvio = dataEnvio;
+    }
 }
